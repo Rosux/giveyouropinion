01a1ee7c9b5005d1c651ffe65351f4d6a6ab6765
RikZuidam <rhj.zuidam@gmail.com>
2022-12-08 19:49:53 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('login', function() {
+    return view('auth.login');
+})->name('login');
+
+Route::post('login', [AdminController::class, 'login']);
+
+Route::get('register', function() {
+    return view('auth.register');
+})->name('register');
+
+Route::post('register', [AdminController::class, 'register']);
+
+Route::prefix('form')->group(function () {
+    Route::get('/', [FormController::class, 'index']);
+
+    Route::get('/create', function () {
+        return view('form/create');
+    });
+
+    Route::post('/create', [FormController::class, 'create']);
+
+    Route::get('/edit', function () {
+        return view('form/edit');
+    });
+
+    Route::post('/edit', [FormController::class, 'edit']);
+
+    Route::get('/show', function () {
+        return view('form/create');
+    });
 });
