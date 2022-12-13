253be58d2b9d172eec36ec32d6b1aa307c2514d0
Rosux <jcsbt2015@gmail.com>
2022-12-13 22:56:54 +0100

    cooler O symbol instead of -

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GYO - Give Your Opinion</title>
+    <title>GYO &#9702; Give Your Opinion</title>
     <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
     <script defer src="{{ asset('scripts/main.js') }}"></script>
     <script src="{{ asset("scripts/DarkMode-min.js") }}"></script>
