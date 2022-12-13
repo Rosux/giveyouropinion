2bb62d6bc48f65b10b8b024b1e8654c3c7954ac1
Rosux <jcsbt2015@gmail.com>
2022-12-13 13:59:07 +0100

    yeet

---
+++ database/migrations/2022_12_13_124738_create_answer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->id();
+            $table->text("userToken");
+            $table->integer("question_id");
+            $table->dateTime("answeredTime");
+            $table->json("answers");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer');
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GYO - Give Your Opium</title>
+    <title>GYO - Give Your Opinion</title>
     <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
 </head>
 <body>
