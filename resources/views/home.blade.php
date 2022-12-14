d66fd677bc603fb8ddadf1be1c3cb507bebc115b
Rosux <jcsbt2015@gmail.com>
2022-12-14 11:55:43 +0100

    more homepage stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,20 @@
         </div>
     </div>
     <div class="wrapper-center">
-        <h2 class="title">Start Your Form Right Here</h2>
+        <h2 class="title">Create your own form</h2>
         {{-- if logged in make button go to create form page else login/register page --}}
         <button>Create New Form</button>
-        yo
+    </div>
+    <div class="hr-main"></div>
+    <div class="wrapper-center theme-bg-dark">
+        <h2 class="title">About GYO</h2>
+        <div class="how-to-wrapper">
+            <div class="how-to-card">
+                <p class="how-to-step-counter">How does it work</p>
+                <p class="how-to-desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis voluptas praesentium voluptatum explicabo doloribus, earum nisi commodi, perspiciatis et nobis accusamus autem iure culpa necessitatibus quae minus. Blanditiis, voluptatibus obcaecati.
+                </p>
+            </div>
+        </div>
     </div>
 </x-layout>
