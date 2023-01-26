3d309b04da24822e3f2e79e8e5a10f09f6bf3b2e
Rosux <jcsbt2015@gmail.com>
2023-01-26 11:37:06 +0100

    form fix

--- resources/views/form/form.blade.php
+++ resources/views/form/form.blade.php
@@ -14,7 +14,39 @@
         @isset($form)
 
             {{-- display form --}}
-            {{ $form }}
+            <?php
+                $form = json_decode($form["questions"], true);
+                
+                // $title = $form["questions"]["title"];
+                // $questions = $form["questions"]["questions"];
+            ?>
+            <h1>{{ $form["title"] }}</h1><br><br><br>
+            <form>
+            <?php
+                for($i=0;$i<count($form["questions"]);$i++){
+                    if($form["questions"][$i]["type"] == 1){
+                        echo("<br><br>");
+                        echo($form["questions"][$i]["question_title"]."<br>");
+                        echo("<input name=".$i." type='text' placeholder='".$form["questions"][$i]["placeholder"]."'/>");
+                    }else if($form["questions"][$i]["type"] == 2){
+                        echo("<br><br>");
+                        echo($form["questions"][$i]["question_title"]."<br>");
+                        for($j=0;$j<count($form["questions"][$i]["choices"]);$j++){
+                            echo("<input name=".$i."  type='radio' value='".$form["questions"][$i]["choices"][$j]."'/>");
+                            echo($form["questions"][$i]["choices"][$j]."<br>");
+                        }
+                    }else if($form["questions"][$i]["type"] == 3){
+                        echo("<br><br>");
+                        echo($form["questions"][$i]["question_title"]."<br>");
+                        for($j=0;$j<count($form["questions"][$i]["choices"]);$j++){
+                            echo("<input name=".$i." type='checkbox' value='".$form["questions"][$i]["choices"][$j]."'/>");
+                            echo($form["questions"][$i]["choices"][$j]."<br>");
+                        }
+                    }
+
+                }
+            ?>
+            </form>
 
         @else
 
